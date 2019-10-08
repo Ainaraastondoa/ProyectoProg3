1e9e8ec75b8c67221badf1a401de390f0b0e78ba
grk_a <grk_a@10.164.58.162>
2019-10-08 12:50:58 +0200

    gorka clases

---
+++ ProyectoProg3/src/elementos/Temporada.java
@@ -0,0 +1,13 @@
+package elementos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Temporada {
+	public static ArrayList<Piloto> pilotos;					//Lista de pilotos
+	public static ArrayList<Integer> puntos;					//Lista de puntos por piloto
+	public static ArrayList<String> circuitos;					//Lista de circuitos
+	public static HashMap<Piloto, Integer> puntosPiloto;		//Puntos que lleva el piloto
+	public static HashMap<Escuderia, Integer> puntosEscuderia;	//Puntos que lleva la escuderia
+
+}
