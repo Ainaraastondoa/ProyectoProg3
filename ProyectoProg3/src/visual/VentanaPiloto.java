ca03586ac69e0f20f597280dedb12058b9e00fb8
Ainara Astondoa <ainara.astondoa@opendeusto.es>
2020-01-10 22:46:00 +0100

    Modificaciones en la ventana Piloto.

--- ProyectoProg3/src/visual/VentanaPiloto.java
+++ ProyectoProg3/src/visual/VentanaPiloto.java
@@ -3,6 +3,7 @@ package visual;
 import java.awt.BorderLayout;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -32,67 +33,66 @@ public class VentanaPiloto extends JFrame{
 	
 	JFrame MenuPrincipalTrayectoria;
 	Piloto piloto; 
+	private JButton bVolver; 
 	
 	public VentanaPiloto(JFrame m) throws SQLException {
-		MenuPrincipalTrayectoria = m; 
+		MenuPrincipalTrayectoria = m;
+		setTitle(" Pilotos ");
+		setSize(m.getWidth(), m.getHeight());
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-		JButton bok = new JButton("Ok");
+		
+		//CREACION DEL PANEL QUE ALBERGA LOS DATOS Y BOTONES
 		JPanel pInferior = new JPanel();
-		JPanel pSuperior = new JPanel();
-		Label etiqueta = new Label("ESCUDERIA");
-		pSuperior.add(etiqueta); 
 		getContentPane().add(pInferior, BorderLayout.SOUTH);
-		getContentPane().add(pSuperior, BorderLayout.NORTH); 
-		pInferior.add(bok);
 		//Creamos dos paneles para cada piloto de la escuderia
 		JPanel pCentral = new JPanel();
 		JPanel pDerecha = new JPanel();
 		JPanel pIzquierda = new JPanel();
-		JPanel pDerechaSupe = new JPanel();
-		JPanel pIzquierdaSupe = new JPanel();
-//		Label eti = new Label("PILOTO 1");
-//		Label eti2 = new Label("PILOTO 2");
-//		pDerechaSupe.add(eti2);
-//		pIzquierdaSupe.add(eti);
-//		pDerecha.add(pDerechaSupe);
-//		pCentral.setLayout(new GridLayout(2,1));
-//		pIzquierda.add(pIzquierdaSupe);
-		pIzquierda.setBackground(Color.DARK_GRAY);
-		pDerecha.setBackground(Color.GRAY);
 		pDerecha.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		pCentral.setLayout(new GridLayout(1,2));
 		pCentral.add(pIzquierda);
 		pCentral.add(pDerecha);
 		getContentPane().add(pCentral);
+		
+		//FUENTE
 		Font font = new Font("Verdana", Font.BOLD, 39);
-		Connection con = BD.initBD("src/datos/F1BaseDatos.db");
-		Statement st = con.createStatement();
+		
+		//OBTENEMOS LOS DATOS DE LOS PILOTOS DE LA ESCUDERIA SELECCIONADA
+		Escuderia escuderia_seleccionada = Trayectoria.getEscuderia();
+		Piloto p1 = escuderia_seleccionada.getPiloto1();
+		Piloto p2 = escuderia_seleccionada.getPiloto2();
+		
+//		Connection con = BD.initBD("src/datos/F1BaseDatos.db");
+//		Statement st = con.createStatement();
 		//ArrayList<Escuderia> escuderias = BD.listaEscuderiasSelect(st);
 		//Imprimimos los datos de los pilotos con sus atributos _PROVISIONAL 
-		Escuderia escuderia_seleccionada = Trayectoria.getEscuderia();
+		
 //		for (Escuderia escuderia : escuderias) {
-//			if (escuderia_seleccionada.equals(escuderia)) {
-				Piloto p1 = escuderia_seleccionada.getPiloto1();
-				Piloto p2 = escuderia_seleccionada.getPiloto2();
-				
-				JTextArea piloto1 = new JTextArea(p1.toString2());
-				piloto1.setEditable( false );
-				piloto1.setBackground(Color.DARK_GRAY);
-				pIzquierda.add(piloto1);
-				piloto1.setFont(font);
-			
-				
-				JTextArea piloto2 = new JTextArea(p2.toString2());
-				piloto2.setEditable( false );
-				piloto2.setBackground(Color.GRAY);
-				pDerecha.add(piloto2);
-				piloto2.setFont(font);
+//			if (escuderia_seleccionada.equals(escuderia)) {	
 //			}
 //		}
+		
+		//IMPRIMIMOS LOS DATOS DEL PRIMER PILOTO
+		JTextArea piloto1 = new JTextArea(p1.toString2());
+		piloto1.setEditable( false );
+		piloto1.setBackground(Color.LIGHT_GRAY);
+		pIzquierda.add(piloto1);
+		piloto1.setFont(font);
 	
+		//IMPRIMIMOS LOS DATOS DEL SEGUNDO PILOTO
+		JTextArea piloto2 = new JTextArea(p2.toString2());
+		piloto2.setEditable( false );
+		piloto2.setBackground(Color.LIGHT_GRAY);
+		pDerecha.add(piloto2);
+		piloto2.setFont(font);
+		
+		//BOTON VOLVER
+		bVolver = new JButton(); 
+		bVolver.setContentAreaFilled(false);
+		bVolver. setOpaque(false);
+		
 		//Escuchadores
-		bok.addActionListener(new ActionListener() {
+		bVolver.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
